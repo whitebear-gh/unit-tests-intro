73dfd78ea079c75e02bb61eb43bd3a05ad6310dd
White Bear <izdebski.krzysztof@gmail.com>
2018-01-23 14:38:44 +0100

    step 2 - Prime service as separate file

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -5,18 +5,13 @@ namespace console_app
     class Program
     {
 
-        public static bool IsPrime(int number){
-            if(number == 1 ) return false;
-            if(number == 2 ) return false;
-            else return true;
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("I want to tell you bro if your number is prime or not");
             var line = Console.ReadLine();
             var number = int.Parse(line);
-            var result = IsPrime(number);
+            var ps = new PrimeService();
+            var result = ps.IsPrime(number);
             if(result){
                 Console.WriteLine("Prime! ");
             }
