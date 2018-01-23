c78fd215a658460c6d0f07a2d3e3c9a7d50c0be0
White Bear <izdebski.krzysztof@gmail.com>
2018-01-23 14:25:11 +0100

    Step 2 - is prime read from console

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -6,7 +6,16 @@ namespace console_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I want to tell you bro if your number is prime or not");
+            var line = Console.ReadLine();
+            var result = int.Parse(line);
+            if(result == 1 ){
+                Console.WriteLine("Prime! ");
+            }
+            else{
+                Console.WriteLine("Not Prime! ");
+            }
+
         }
     }
 }
