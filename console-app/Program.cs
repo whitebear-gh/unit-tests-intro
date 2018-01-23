a245cb0c8f0736359434228b6869c77433a37580
White Bear <izdebski.krzysztof@gmail.com>
2018-01-23 14:30:37 +0100

    Step 3 - Is Prime as separate method

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -4,12 +4,20 @@ namespace console_app
 {
     class Program
     {
+
+        public static bool IsPrime(int number){
+            if(number == 1 ) return false;
+            if(number == 2 ) return false;
+            else return true;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("I want to tell you bro if your number is prime or not");
             var line = Console.ReadLine();
-            var result = int.Parse(line);
-            if(result == 1 ){
+            var number = int.Parse(line);
+            var result = IsPrime(number);
+            if(result){
                 Console.WriteLine("Prime! ");
             }
             else{
